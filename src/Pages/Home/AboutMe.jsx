4c332c6b72ac01b46bc72a596e87f5b69ff8f493
Aimable Mugwaneza <aimable.mugwaneza@gmail.com>
2025-08-06 16:26:04 -0400

    -Updated about me section and badges

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -84,15 +84,48 @@ export default function AboutMe() {
             </p>
             
             <p className="about-description">
-              This journey has reignited my drive and purpose. I think in systems now, sketching out ideas at night, imagining how components connect, and constantly applying what I learn in real-world scenarios. I've earned my AWS Cloud Practitioner and Solutions Architect Associate , Security+ and Terraform Associate, and I'm building public, documented projects to show what I know — not just what I've studied.
+              This journey has reignited my drive and purpose. I think in systems now, sketching out ideas at night, imagining how components connect, and constantly applying what I learn in real-world scenarios. I've earned my AWS Cloud Practitioner and Solutions Architect Associate , Security+ and Terraform Associate, and I'm building public, documented projects to show what I know and not just what I've studied.
             </p>
             
             <p className="about-description">
-              Cloud and DevOps blend everything I love: infrastructure, automation, security, creativity, and impact. It's less saturated, highly in demand, and rewards those who can bridge software with systems — and that's where I thrive. I'm not just following a path — I'm creating one. And I'm just getting started.
+              Cloud and DevOps blend everything I love: infrastructure, automation, security, creativity, and impact. It's less saturated, highly in demand, and rewards those who can bridge software with systems and that's where I thrive. I'm not just following a path, I'm creating one. And I'm just getting started.
             </p>
 
           {/* Certification badge placeholder */}
-            
+            <div className="cert-badge-container">
+                <img
+                  onClick={() => {
+                              window.open("./CompTIA_Security+_CE.pdf", '_blank', 'noopener noreferrer')
+                            }}
+                  src="./img/security-plus.png"
+                  alt="CompTIA Security+ Certified"
+                  className="cert-badge-image"
+                />
+                <img
+                onClick={() => {
+                              window.open("./AWS_Certified_Cloud_Practitioner.pdf", '_blank', 'noopener noreferrer')
+                            }}
+                  src="./img/aws-ccp.png"
+                  alt="AWS Certified Cloud Practitioner"
+                  className="cert-badge-image"
+                />
+                <img
+                  onClick={() => {
+                              window.open("./AWS_Certified_Solutions_Architect.pdf", '_blank', 'noopener noreferrer')
+                            }}
+                  src="./img/aws-certified-solutions-architect-associate.png"
+                  alt="AWS Certified Solutions Architect - Associate"
+                  className="cert-badge-image"
+                />
+                <img
+                  onClick={() => {
+                              window.open("./TerraformAssociate00320250716-27-5e9c2p.pdf", '_blank', 'noopener noreferrer')
+                            }}
+                  src="./img/terraform.png"
+                  alt="Terraform Associate Certified "
+                  className="cert-badge-image"
+                />
+              </div>
             
           </div>
         </div>
